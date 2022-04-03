a022da58913c17b0a809b6790009f07d88f06eec
Tarcaxoxide <loganer@vivaldi.net>
2022-04-03 12:25:19 -0400

    Sun Apr  3 12:25:19 PM EDT 2022

--- shelldir/calculate.sh
+++ shelldir/calculate.sh
@@ -1,5 +1,9 @@
 #!/bin/env bash
 
-PreCalculate='pi=3.14159265358979323846;e=2.71828182845904523536'
+PreCalculate='pi=3.14159265358979323846;\
+              e=2.71828182845904523536;\
+              minute=60;\
+              hour=minute*60;\
+              day=hour*24'
 
 echo "$* =$(calc "$PreCalculate;$*")"
