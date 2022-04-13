8c1c24981b300148e4edb55bcfed55150d5bd557
Tarcaxoxide <loganer@vivaldi.net>
2022-04-13 01:03:01 -0400

    Wed Apr 13 01:03:01 AM EDT 2022

--- pythonGarbage/REEEEEEE.py
+++ pythonGarbage/REEEEEEE.py
@@ -5,8 +5,8 @@ from mi import *
 import time
 import Credential
 time.sleep(10)
-ip='127.1.0.1'
-port=5400
+IP='127.1.0.1'
+PORT=5400
 ADDR=(IP,PORT)
 print(f'Connecting to socket {ADDR}...')
 zclient = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
@@ -38,8 +38,8 @@ class MyBot(commands.Bot):
     async def on_ready(self, ws):
         print("work on my machine")
         await Router(ws).connect_channel(channels)  # globalとmainチャンネルに接続
-        #self.task.start()  # タスクを起動する
-        #self.task.stop()  # タスクを止める
+        self.task.start()  # タスクを起動する
+        self.task.stop()  # タスクを止める
 
     async def on_message(self, note: Note):
         instance_name = note.author.instance.name if note.author.instance else 'local'
@@ -47,7 +47,8 @@ class MyBot(commands.Bot):
 
         if note.author.name != 'MeSoBot':
             MSGZ = await send(note.content)
-            res = await note.reply(str(MSGZ))
+            print(f'?{MSGZ}?')
+            res = await note.reply(str(MSGZ)) #<< it doesn't seem to be able to send the message?
             print(f'?{MSGZ}?')
 
 if __name__ == "__main__":
