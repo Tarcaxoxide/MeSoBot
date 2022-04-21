c3c86292801d42536e0bbd379c8e6ba16efddd94
Tarcaxoxide <loganer@vivaldi.net>
2022-04-21 15:06:44 -0400

    Thu Apr 21 03:06:44 PM EDT 2022

--- inc/LangaugeProcessing.hpp
+++ inc/LangaugeProcessing.hpp
@@ -6,17 +6,24 @@
 
 namespace PROGRAM_NAME{
     struct Sentence_st{
+        struct next_word_st{
+            std::string word;
+            int64_t likelihood;
+        };
         struct word_st{
-            std::string word; // the word itself.
-            int64_t amount; // the amount of times this word occurs.
+            std::string word;
+            int64_t amount;
+            std::deque<next_word_st> next_words;
         };
         std::deque<word_st> words; // the words in the sentence.
 
-        void add(std::string target_word);
+        void add(std::string target_word,std::string next_word="");
         Sentence_st(std::string new_string);
+        void AddSentence(std::string new_string);
         std::string to_string();
         int64_t findWordAmount(std::string target);
         std::string findWordString(size_t index);
+        std::string Random(size_t WordCount=10);
         unsigned int operator==(Sentence_st &other);
     };
 };
--- src/LangaugeProcessing.cpp
+++ src/LangaugeProcessing.cpp
@@ -1,24 +1,46 @@
 #include<LangaugeProcessing.hpp>
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>       /* time */
 
 namespace PROGRAM_NAME{
-    void Sentence_st::add(std::string target_word){
+    void Sentence_st::add(std::string target_word,std::string next_word){
         size_t hasWord=0;
         for(size_t i=0;i<words.size();i++){
             if(words[i].word == target_word)hasWord=i+1; 
         }
+        next_word_st NextWord;
+        if(next_word.size()){
+            NextWord={next_word,1};
+        }else{
+            NextWord={next_word,0};// 0% likely , aka not possible.
+        }
         if(hasWord == 0){
-            words.push_back({target_word,1});
+            words.push_back({target_word,1,{NextWord}});
         }else{
             words[hasWord-1].amount++;
+            size_t hasNextWord=0;
+            for(size_t i=0;i<words[hasWord-1].next_words.size();i++){
+                if(words[hasWord-1].next_words[i].word == next_word)hasNextWord=i+1;
+            }
+            if(hasNextWord == 0){
+                words[hasWord-1].next_words.push_back(NextWord);
+            }else{
+                words[hasWord-1].next_words[hasNextWord-1].likelihood++;
+            }
         }
     }
     Sentence_st::Sentence_st(std::string new_string){
+        AddSentence(new_string);
+    }
+    void Sentence_st::AddSentence(std::string new_string){
         if(new_string[new_string.size()-1] != ' ')new_string+=' ';
         std::string buffer="";
+        std::string oldBuffer="";
         for(size_t i=0;i<new_string.size();i++){
             if(new_string[i] != '\n'){
                 if(new_string[i] == ' '){
-                    add(buffer);
+                    add(oldBuffer,buffer);
+                    oldBuffer=buffer;
                     buffer="";
                 }else{
                     buffer+=new_string[i];
@@ -43,6 +65,34 @@ namespace PROGRAM_NAME{
     std::string Sentence_st::findWordString(size_t index){
         if(index > words.size()-1)return " ";
         return words[index].word;
+    }
+    std::string Sentence_st::Random(size_t WordCount){
+        std::string buffer="",NextWord="",PreviewsWord="";
+        size_t oldIndex=0;
+        for(size_t a=0;a<WordCount;a++){
+            bool hasNot=true;
+            size_t ra = rand() % words.size();
+            size_t X=0;
+            while(hasNot){
+                ra = rand() % words.size();
+                for(size_t z=0;z<words[oldIndex].next_words.size();z++){
+                    if(findWordString(ra) == words[oldIndex].next_words[z].word){
+                        hasNot=false;
+                        if(words[oldIndex].next_words[z].likelihood > X){
+                            NextWord=words[oldIndex].next_words[z].word;
+                            X=words[oldIndex].next_words[z].likelihood;
+                            std::cout<<"?"<<PreviewsWord<<":"<<X<<":"<<NextWord<<"?"<<std::endl;
+                        }
+                    }
+                }
+                if(oldIndex == 0)hasNot=false;
+                oldIndex=ra;
+            }
+            PreviewsWord=NextWord;
+            buffer+=NextWord+std::string(" ");
+        }
+        
+        return buffer;
     }
     unsigned int Sentence_st::operator==(Sentence_st &other){
         int64_t buffer=0;
